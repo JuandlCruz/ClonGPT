eb532ca16d485ccbb4441414620cdc78e33690d9
JuandlCruz <juandelacrouz@gmail.com>
2023-03-29 10:17:52 +0200

    Initial Commit

---
+++ ClonGPT/ClonGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClonGPTApp.swift
+//  ClonGPT
+//
+//  Created by JSANCAGU on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ClonGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
